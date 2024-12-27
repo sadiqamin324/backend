02883cbd272e452cb1f835c51ad4e07a5b6ebb01
sadiq amin <sadiqamin324@gmail.com>
2024-12-27 21:43:39 +0500

    changes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 //  promises
  const asyncHandler =(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(req,res,next).catch((err)=>next(err))
     }
         
