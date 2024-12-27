3ee4151f28d5bafc48bacf2adf285a5d85e5e701
sadiq amin <sadiqamin324@gmail.com>
2024-12-27 19:59:08 +0500

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from"jsonwebtoken"
 
+import bcrypt from "bcrypt"
 const userSchema = new Schema(
   {
     username: {
@@ -36,6 +38,43 @@ const userSchema = new Schema(
     timestamps: true, // Added here as part of the Schema options
   }
 );
+userSchema.pre("save",async function name(next) {
+    if (!this.isModified("password")) return next();
+        
+    
+    this.password=bcrypt.hash(this.password,10)
+    next()
+    
+})//we are encrypting our data before saving
+userSchema.methods.isPasswordCorrect= async function (password) { return await bcrypt.compare(password,this.password)
+  
+}
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+          email: this.email,
+          username: this.username,
+          fullName: this.fullName
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+          expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+  )
+}
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+          
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      }
+  )//ismain info kam hoti hay
+}
 
 export default mongoose.model("User", userSchema);
 
