e7095492c7db7b872131dbfbd8eae967bf7a5597
sadiq amin <sadiqamin324@gmail.com>
2024-12-28 16:11:59 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 //require('dotenv').config({path:'./env'})//
-import { app } from "./app.js";
-import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+
+
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT|| 800),()=>{
-        console.log(`server is running at port"${process.env.PORT}`);
-    }
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
-.catch((err)=> {
-    console.log('error'.err);
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
 /* import mongoose from "mongoose";
  import { DB_NAME } from "./constants";
  (async()=>
